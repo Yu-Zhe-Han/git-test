a430bba559fcac1411055c0494438c0423a4cdd6
YZH <813776531@qq.com>
2022-07-19 22:57:26 +0800

    Second commit

--- src/main/java/com/yzh/git/GitTest.java
+++ src/main/java/com/yzh/git/GitTest.java
@@ -2,6 +2,7 @@ package com.yzh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-       System.out.println("hello,yzh");
+       System.out.println("hello,yzh1");
+        System.out.println("hello,yzh2");
     }
 }
