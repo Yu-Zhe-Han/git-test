23fb72c336a0575ae119adc13d283ad4cc2e44fb
YZH <813776531@qq.com>
2022-07-20 16:01:13 +0800

    Hot-fix commit

--- src/main/java/com/yzh/git/GitTest.java
+++ src/main/java/com/yzh/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
        System.out.println("hello,yzh1");
         System.out.println("hello,yzh2");
         System.out.println("hello,yzh3");
+        System.out.println("hello,yzh4");
     }
 }
