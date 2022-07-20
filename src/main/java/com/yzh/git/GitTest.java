1a67a110c0aa12ec6fcc349541948536c8869d43
YZH <813776531@qq.com>
2022-07-20 16:05:41 +0800

    Master commit2

--- src/main/java/com/yzh/git/GitTest.java
+++ src/main/java/com/yzh/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,yzh2");
         System.out.println("hello,yzh3");
         System.out.println("hello,yzh4");
+        System.out.println("msater test");
     }
 }
