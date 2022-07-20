0989748e04a1264c8865ef1dcbef979294b6f789
YZH <813776531@qq.com>
2022-07-20 16:23:44 +0800

    push commit2

--- src/main/java/com/yzh/git/GitTest.java
+++ src/main/java/com/yzh/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,yzh4");
         System.out.println("msater test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
